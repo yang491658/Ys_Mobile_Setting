82fb19b7b185b40f433292caabfe1da26c5fe324
yang491658 <park49164357@gmail.com>
2025-10-23 16:19:55 +0900

    Update SoundManager.cs

--- Assets/Scripts/Managers/SoundManager.cs
+++ Assets/Scripts/Managers/SoundManager.cs
@@ -70,9 +70,6 @@ public class SoundManager : MonoBehaviour
             btn.onClick.AddListener(Button);
     }
 
-    private void OnApplicationPause(bool _pause) => AudioListener.pause = _pause;
-    private void OnApplicationFocus(bool _hasFocus)=> AudioListener.pause = !_hasFocus;
-
     #region πË∞Ê¿Ω
     public void PlayBGM(AudioClip _clip)
     {
@@ -149,9 +146,6 @@ public class SoundManager : MonoBehaviour
         bgmSource.playOnAwake = false;
         sfxSource.playOnAwake = false;
         bgmSource.loop = true;
-
-        bgmSource.ignoreListenerPause = false;
-        sfxSource.ignoreListenerPause = false;
 
         SetBGMVolume(bgmVol);
         SetSFXVolume(sfxVol);
